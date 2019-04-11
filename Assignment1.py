b7390dd558187734e066074b78969dfe1d502793
TanzeemAlam <Tanzeemalam789@gmail.com>
2019-04-11 17:08:31 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,17 +1,20 @@
 #print the second element from array
 fruits = ["apple", "banana", "cherry"]
 print(      )
-
+print(fruits[2])
 ____________________________________________________
 
 #print multiple 5 till 10 by using range
 #do same as I have done by printing multiple of 2 using range(2,10,2)
-
-____________________________________________
+for i in range(5,11,5):
+  print(i)
+___________________________________________
 
 #Write a python code to search 'j' in array x and print YES if available or else NO
 x=['f','g','t','h','j','w','k','p']
-
+i = 'j'
+if i in x:
+  print("Yes")
 ______________________________________________
 
 #Change the value from "apple" to "kiwi", in the x.
@@ -19,7 +22,7 @@ ______________________________________________
 #HINT --> use assignment operator
 
 x=["apple","orange","banana","grapes"]
-
+x[0] = "kiwi"
 _ _ _ _ _ = _ _ _ _ _
 
 _____________________________________________________________________
